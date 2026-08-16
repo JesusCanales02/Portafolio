c903d85559bad0f0d609f3976259bbf14bd6a646
Jesus Canales <jadrian4b@gmail.com>
2026-08-15 23:53:48 -0700

    Se movio los titulos del navBar en el centro para eliminar el nombre repetitivo

--- src/componentes/Navbar/Navbar.jsx
+++ src/componentes/Navbar/Navbar.jsx
@@ -1,25 +1,23 @@
 import "./Navbar.css";
-function Navbar() {
-    return(
-        <>
-        <nav className="navbar">
-            <h2 className="logo">Jesús Canales</h2>
 
+function Navbar() {
+    return (
+        <nav className="navbar">
             <ul className="nav-links">
                 <li>
                     <a href="#home">Inicio</a>
                 </li>
+
                 <li>
                     <a href="#projects">Proyectos</a>
                 </li>
+
                 <li>
                     <a href="#contact">Contacto</a>
                 </li>
             </ul>
         </nav>
-        </>
-    )
-    
+    );
 }
 
-export default Navbar
+export default Navbar;
